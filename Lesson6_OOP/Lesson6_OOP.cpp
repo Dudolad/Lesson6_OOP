b29367d54ff0594af1f1b82666e0c1798584526d
dudolad_yaroslav <dudolad2009@gmail.com>
2025-09-09 18:33:33 +0300

    main

--- Lesson6_OOP/Lesson6_OOP.cpp
+++ Lesson6_OOP/Lesson6_OOP.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "Date.h"
+#include "Product.h"; 
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -7,7 +9,15 @@ int main()
 	Date d1;
 	Date d2(2026, 1, 1);
 
-	d1++;
+	Product a;
+	cin >> a;
+	
+	cout << "------------------------------------------------\n";
+
+	Product b("Milk", 50.90, Date(2025, 9, 1), 3);
+	cout << a << endl << b;
+
+	/*d1++;
 	d2--;
 	d1.showDate();
 	d2.showDate();
@@ -20,5 +30,5 @@ int main()
 	cin >> d3;
 	cout << d3 << endl;
 	cout << d3 << " + " << 5 << " = " << d3 + 5 << endl;
-	cout << 5 << " + " << d3 << " = " << 5 + d3 << endl;
+	cout << 5 << " + " << d3 << " = " << 5 + d3 << endl;*/
 }
