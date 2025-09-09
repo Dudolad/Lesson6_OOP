e883993fe7dcd01094fcd62beba7769cc982d495
dudolad_yaroslav <dudolad2009@gmail.com>
2025-09-09 18:43:03 +0300

    shop

---
+++ Lesson6_OOP/Shop.cpp
@@ -0,0 +1,46 @@
+#include "Shop.h"
+
+Shop::Shop()
+{
+	arr.push_back(Product("Milk", 50.90, Date(2025, 9, 1), 3));
+	arr.push_back(Product("Crisps", 70.90, Date(2025, 2, 10), 100));
+	arr.push_back(Product("Bread", 27.00, Date(2025, 5, 8), 4));
+
+}
+
+Product Shop::operator[](string title)
+{
+	return Product();
+}
+
+Product& Shop::operator[](int n)
+{
+	// TODO: insert return statement here
+}
+
+void Shop::addProduct(Product p)
+{
+	arr.push_back(p);
+}
+
+float Shop::calcExpiredProducts() const
+{
+	return 0.0f;
+}
+
+void Shop::updateDiscountProducts()
+{
+}
+
+void Shop::delExpiredProducts()
+{
+}
+
+void Shop::showInfo() const
+{
+	cout << "Amount of products: " << arr.size() << endl;
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
---
+++ Lesson6_OOP/Shop.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include "Product.h"
+class Shop
+{
+	vector<Product> arr;
+public:
+	Shop();
+
+	Product operator[](string title);
+	Product& operator[](int n);
+
+	void addProduct(Product p);
+	float calcExpiredProducts() const;
+	void updateDiscountProducts();
+	void delExpiredProducts();
+
+	void showInfo()const;
+
+};
+
